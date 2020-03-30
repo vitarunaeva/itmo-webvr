9c7ed419fdae0a48b6bfcff025fa061aa6824f85
Violetta Runaeva <runaevavioletta@gmail.com>
2020-03-30 22:35:42 +0300

    change event menu button

--- src/components/menu-button.js
+++ src/components/menu-button.js
@@ -1,6 +1,6 @@
 AFRAME.registerComponent("menubutton", {
   events: {
-    mouseenter: function() {
+    click: function() {
       this.el.setAttribute("scale", "0 0 0");
       document.querySelector("#menu").setAttribute("scale", "1 1 1");
     }
